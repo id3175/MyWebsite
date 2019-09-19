75149da62a0e005de3e83d47350fbb2880533e64
Kaine-UCF <kaine.margretta@knights.ucf.edu>
2019-09-19 19:12:55 -0400

    Website

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+     My Testing Ground for Full Stack Software Development
+    </footer>
+  );
+};
